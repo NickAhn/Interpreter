f6994f9423352c1f11793802610383939e6cd4f5
Nick Ahn <n_ahn2@u.pacific.edu>
2022-02-03 01:02:24 -0800

    added read/write code for input and output txt files)

--- project-1/main.py
+++ project-1/main.py
@@ -1,14 +1,14 @@
-def fizzbuzz(n):
-    s = ""
-    if n % 3 == 0:
-        s += "fizz"
-    if n % 5 == 0:
-        s += "buzz"
+import sys
 
-    if s == "":
-        print(n)
-    else:
-        print(s)
+# Run program as: "python3 main.py <input-file> <output-file>"
+try:
+    input_list = open(sys.argv[1]).read().splitlines()
+    output_file = open(sys.argv[2], "w")
+except:
+    print("Failed to open file(s).")
+    sys.exit(1)
 
-for i in range (100):
-    fizzbuzz(i)
+
+# output
+for x in input_list:
+    output_file.write("Line: " + x + "\n")
