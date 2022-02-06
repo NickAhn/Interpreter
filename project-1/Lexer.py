79e5cde7eef7b360138bfba1e5eb052b446ff4f6
Nick Ahn <n_ahn2@u.pacific.edu>
2022-02-06 13:38:36 -0800

    starting to code Lexer.scan() function

--- project-1/Lexer.py
+++ project-1/Lexer.py
@@ -5,3 +5,22 @@ class Lexer:
             "NUMBER" : "[0-9]+",
             "SYMBOL" : "\+ | \- | \* | / | \( | \)",    
         }
+
+
+    def scan(list: input):
+        input_list = input.split() # separate inputs by delimiters (whitespaces)
+        for input in input_list:
+            currTokenPos = 0 #keep track of beginning of the token being analyzed #WE MIGHT NOT NEED THIS!
+            currToken = ""
+            for i in range(len(input)):
+                currToken += input[i]
+                if i == 0:
+                    # prevTokenType = getTokenType of input[i] #WE MIGHT NOT NEED THIS!
+                    continue
+                
+                '''if getTokenType(currToken, self.t_tpes):
+                        
+                    else:
+                        print(getTokenType(currToken[:-1]), ": ", currToken[:-1])
+                        currToken = ""
+                '''
