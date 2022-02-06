6dca04e1954416b33ac8b2205faebe968806d5af
emilyyeh123 <e_yeh1@u.pacific.edu>
2022-02-06 14:32:28 -0800

    added getTokenType()

--- project-1/Lexer.py
+++ project-1/Lexer.py
@@ -1,7 +1,19 @@
+import re
+
+
 class Lexer:
     def __init__(self) -> None:
         self.t_types = {
-            "IDENTIFIER" : "([a-z]|[A-Z]|)([a-z]|[A-Z]|[0-9])",
-            "NUMBER" : "[0-9]+",
-            "SYMBOL" : "\+ | \- | \* | / | \( | \)",    
+            "IDENTIFIER": "([a-z]|[A-Z]|)([a-z]|[A-Z]|[0-9])",
+            "NUMBER": "[0-9]+",
+            "SYMBOL": "\+ | \- | \* | / | \( | \)",
         }
+
+    # check which token type current buffer is
+    def getTokenType(self, buffer):
+        token_type = ""
+        for key, val in self.t_types:
+            match = re.match(val, buffer)
+            if match:
+                return key
+        return "INVALID TOKEN"
