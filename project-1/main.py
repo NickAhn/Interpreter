544f2dd7e9448f5dd81bc86cf3419d10fc3fee18
Nick Ahn <n_ahn2@u.pacific.edu>
2022-02-06 13:21:47 -0800

    added regex rules and imported re library

--- project-1/main.py
+++ project-1/main.py
@@ -1,14 +1,22 @@
-import sys
+import sys 
+import re #regex library
 
 # Run program as: "python3 main.py <input-file> <output-file>"
-try:
-    input_list = open(sys.argv[1]).read().splitlines()
-    output_file = open(sys.argv[2], "w")
-except:
-    print("Failed to open file(s).")
-    sys.exit(1)
+# try:
+#     input_list = open(sys.argv[1]).read().splitlines()
+#     output_file = open(sys.argv[2], "w")
+# except:
+#     print("Failed to open file(s).")
+#     sys.exit(1)
 
 
-# output
-for x in input_list:
-    output_file.write("Line: " + x + "\n")
+# # output
+# for x in input_list:
+#     output_file.write("Line: " + x + "\n")
+
+
+token_types = {
+    "IDENTIFIER" : "([a-z]|[A-Z]|)([a-z]|[A-Z]|[0-9])",
+    "NUMBER" : "[0-9]+",
+    "SYMBOL" : "\+ | \- | \* | / | \( | \)",    
+}
